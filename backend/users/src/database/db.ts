db13c9a539dea9aeb881dc3799823235bfa683f2
tarak suthar <tarak.suthar1999@gmail.com>
2022-04-26 21:52:20 +0530

    setup server

---
+++ backend/users/src/database/db.ts
@@ -0,0 +1,3 @@
+class Db{
+    private _url:
+}
--- backend/users/src/index.ts
+++ backend/users/src/index.ts
@@ -1,16 +1,15 @@
-import express from 'express'
-import YAML from 'yamljs'
-import swaggerUi from 'swagger-ui-express'
+import express from 'express';
+import console from 'console';
+import userRouter from './routes/user.route';
 
-const app = express()
+const app = express();
 // const swaggerDocument = YAML.load('../swagger.yaml'); TODO: r&d swagger
 // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-const userRouter = require('./routes/user.route')
-app.use(userRouter)
-app.use('/*',()=>{
-    console.log("Not found");
+app.use(userRouter);
+app.use('/*', () => {
+  console.log('Not found');
 });
 
-app.listen(3080,()=>{
-    console.log('server up and running...')
-})
+app.listen(3080, () => {
+  console.log(`--- Spinning on 3080 with dev environment 💖 ---`);
+});
